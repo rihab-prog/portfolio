0973511c5ff21c8973a76c8218e27a84d3f09baf
rihab-prog <76817020+rihab-prog@users.noreply.github.com>
2021-01-01 02:09:18 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8 /">
+        <link rel="stylesheet" href="portfolio.css"/>
+        <title>Portfolio</title>
+    </head>
+    <body> 
+        <style>
+            body {
+                 background-image: url('fem.png');
+            }   
+        </style>
+        <header>
+            <h1>Bienvenue dans mon Portfolio!</h1>
+            <h2 id="bonjour">
+                <p1>Je suis Rihab AMMAR, developpeuse web à la recherche d'alternance</p1> 
+            </h2>
+            <div class="lien">
+                <a href="aboutme.php?page=A propos de moi" class="bouton">Accéder au Portfolio</a>
+            </div>        
+        </header>   
+    </body>
+
+</html>
